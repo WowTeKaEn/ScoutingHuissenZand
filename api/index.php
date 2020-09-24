1186c5090234f41f636727422f3264cfa4bc5c64
Wout Knuiman <WT.Knuiman@student.han.nl>
2020-09-24 23:39:39 +0200

    'publish'

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN'] );
+header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN'] );
 header("Access-Control-Allow-Credentials: true");
 
 $method = $_SERVER['REQUEST_METHOD'];
